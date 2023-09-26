6a37c74c43c2a5647d440c990e2a82c564215a0f
Hellen Caroline <azev.hellen@gmail.com>
2023-09-25 21:09:13 -0300

    Initial Commit

---
+++ rick-and-morty-characters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty-characters
+//
+//  Created by Hellen Caroline  on 25/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
